37a424392b66bb51b76092e5cd947ccd7703d1cb
celery1124 <celery1124@tamu.edu>
2018-03-13 15:07:50 -0500

    change mind to use optget for parsing parameters, since it's easy to manage print to stderr

--- aggiestack.py
+++ aggiestack.py
@@ -1,8 +1,25 @@
-import sys
-import argparse
+import getopt, sys
 
 def eprint(*arg):
 	print >>sys.stderr, arg
 
+def usage():
+	print "will print usage later"
 
 
+def main():
+	try:
+		cmd = sys.argv[1]
+	except:
+		usage()
+		sys.exit()
+	try:
+		opts, args = getopt.getopt(sys.argv[2:], "h", ["hardware=", "images=", "flavors="])
+	except getopt.GetoptError as err:
+		eprint(str(err))  # will print something like "option -a not recognized"
+		usage()
+		sys.exit()
+
+	print opts
+if __name__ == "__main__":
+    main()
